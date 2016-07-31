e18304b222759a67365365707ea1ca121229bb39
jim <876142347@qq.com>
2016-07-31 21:54:41 +0800

    linux

--- simpleServer.py
+++ simpleServer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #encoding:utf-8
 import socket   #socket模块
+import time 
 
 HOST='127.0.0.1'
 PORT=50007
@@ -20,17 +21,17 @@ while 1:
 
 
         conn,addr=s.accept()   #接受TCP连接，并返回新的套接字与IP地址
-        #conn.setblocking(0)
+        conn.setblocking(0)
         #s = None
         print'Connected by',addr ,conn    #输出客户端的IP地址
         list_client.append(conn)
     except Exception as e:
         #print e
         pass
-    print list_client
+    #print list_client
     for pos in xrange(len(list_client)):
-        print len(list_client)
-        print pos
+        #print len(list_client)
+        #print pos
         conn = list_client[pos]
 
         time.sleep(0.1)
@@ -38,8 +39,8 @@ while 1:
             data=conn.recv(1024)    #把接收的数据实例化
             if len(data)==0:
                 conn.close()
-            print len(data)
-            print data
+            # print len(data)
+            #print data
             
 
             if data =='close':
